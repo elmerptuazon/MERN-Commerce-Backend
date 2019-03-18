399047e5c17df6caabc9b6dead3e22c07d24bf80
Elmer <elmerptuazon@yahoo.com>
2019-03-18 17:19:45 +0800

    sync frontend inc

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const shopRoutes = express.Router();
 const cors = require("cors");
 const PORT = 4000;
+
+let Shop = require("./shop.model");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,6 +17,30 @@ const connection = mongoose.connection;
 connection.once("open", function() {
   console.log("MongoDB Connected");
 });
+
+shopRoutes.route("/").get(function(req, res) {
+  Shop.find(function(err, shop) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(shop);
+    }
+  });
+});
+
+shopRoutes.route("/edit/profile").post(function(req, res) {
+  let shop = new Shop(req.body);
+  shop
+    .save()
+    .then(shop => {
+      res.status(200).json({ shop: "item has been added" });
+    })
+    .catch(err => {
+      res.status(400).send * "shop item failed";
+    });
+});
+
+app.use("/shop", shopRoutes);
 
 app.listen(PORT, function() {
   console.log("Server Good");
--- shop.model.js
+++ shop.model.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.schema;
+const Schema = mongoose.Schema;
+
+let Shop = new Schema({
+  shop_file: {
+    type: Object
+  },
+  shop_title: {
+    type: String
+  },
+  shop_price: {
+    type: String
+  },
+  shop_completed: {
+    type: Boolean
+  }
+});
+
+module.exports = mongoose.model("Shop", Shop);
